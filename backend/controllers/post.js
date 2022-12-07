cbfdaad7d15461359f5ea1d3cbe5c6c93cdba761
Reafort <rearicafort21@gmail.com>
2022-12-06 17:56:39 -0800

    createpost controller

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,27 +1,29 @@
 const { Post } = require('../models/post');
 
 
+
 /*post function */
 //TODO look up project 6 how sauces are created (create and modify sauces) changing mongoose to sequelize.
 exports.createPost = (req, res, next) => {
     let body = JSON.parse(req.body.post);
-        const post = new Post({
-            message: body.message,
-            userId: body.userId,
-            imageUrl: body.imageUrl
-        });
-        console.log(post)
-        user.save().then(
-            () => {
-                res.status(201).json({
-                    message: "Post added sucessfully!"
-                });
-            }
-        ).catch(
-            (error) => {
-                res.status(500).json({
-                    error: error
-                });
-            }
-        )
-    }
+    // const url = req.protocol + '://' + req.get('host');
+    const post = new Post({
+        message: body.message,
+        userId: body.userId,
+        // imageUrl: url + '/images/' + req.file.filename,
+    });
+    console.log(post)
+    user.save().then(
+        () => {
+            res.status(201).json({
+                message: "Post added sucessfully!"
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(500).json({
+                error: error
+            });
+        }
+    )
+}
